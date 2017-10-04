6c7e2b837de7d7325187319689f0c2aff13b1f62
Jeff Ostrom <j.ostrm@gmail.com>
2017-10-04 15:27:46 -0500

    updates JS loop images/If Else/moved var out of document.ready function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,81 +1,71 @@
+
+    var wins = 0;
+    var losses = 0;
+    var match = 0;
+    var totalScore = 0;
+//Move var outside document ready function!
 $(document).ready(function(){
+    
+    match = randomNumber(19, 120);
+    $("#random").html(match); // displays to screen
+    
+    //sets the images for the crystal buttons
+    for(var i = 1; i <= 4; i++){
+        $("#crystal"+i).html("<img src='assets/Images/crystal"+i+".png' alt='picture of a crystal'>");
+    };
 
-	
+    //sets the crystal's value
+    randomCrystalValues();
 
-	var wins = 0;
-	var losses = 0;
-	var match = 0;
-	var totalScore = 0;
-
-	match = randomNumber(19, 120);
-	$("#random").html(match); // displays to screen
-
-
-	$("#crystalA").attr("data-points", randomNumber(1,12));
-	$("#crystalB").attr("data-points", randomNumber(1,12));
-	$("#crystalC").attr("data-points", randomNumber(1,12));
-	$("#crystalD").attr("data-points", randomNumber(1,12));
-
-
-	//RANDOM NUMBER FUNCTION
-	function randomNumber(min , max)
-	{
-		return Math.round( (Math.random()*(max-min)) + min);     
-	}
-
-	function reStart(){
-		
-		match = randomNumber(19, 120);
-		$("#random").html(match);
-		console.log(random);
-		totalScore = 0;
-		$("#totalScore").html(totalScore);	
-
-
-		$("#crystalA").attr("data-points", randomNumber(1,12));
-		$("#crystalB").attr("data-points", randomNumber(1,12));
-		$("#crystalC").attr("data-points", randomNumber(1,12));
-		$("#crystalD").attr("data-points", randomNumber(1,12));		
-
-	}
-
-
-	// attaches on "click" event handler to every element with class ".crystal"
-	$(".crystal").on("click", function(){
-
-		var points = parseInt($(this).attr("data-points"));
-
-		totalScore = totalScore + points;
-		$("#yourScore").html(totalScore);
-
-		//compare to total score to match
-		if(totalScore === match)
-		{
-			// I need to fix this to reset once win!
-			wins = wins + 1;
-			//I need to figure out how to enter alert Wins/Loss
-			console.log("You win!");
+    // attaches on "click" event handler to every element with class ".crystal"
+    $(".crystal").on("click", function(){
+        var points = parseInt($(this).attr("data-points"));
+        totalScore = totalScore + points;
+        $("#yourScore").html(totalScore);
+        //compare to total score to match
+        if(totalScore === match)
+        {
+            // I need to fix this to reset once win!
+            wins = wins + 1;
+            //I need to figure out how to enter alert Wins/Loss
+            console.log("You win!");
         
-	        $("#wins").html(wins + "<p>You win!</p>");
-			// update wins on screen
-			reStart();
-			//Restarts
-		}
-		
-		if(totalScore > match)
-		{
-			//I need to reset this when lose.
-			losses = losses + 1;
-
-			// display you losses 
-			  $("#loses").html(losses + "<p>You Lost!</p>");
-			// update loses on screen
-			reStart();
-			//Restarts
-		}
-
-
-
-	});
+            $("#wins").html(wins + "<p>You win!</p>");
+            // update wins on screen
+            reStart();
+            //Restarts
+        } else if(totalScore > match) {
+            //I need to reset this when lose.
+            losses = losses + 1;
+            // display you losses 
+              $("#loses").html(losses + "<p>You Lost!</p>");
+            // update loses on screen
+            reStart();
+            //Restarts
+        }
+    });
 });
 
+//set random values to the crystals
+function randomCrystalValues(){
+    for(var i = 1; i <= 4; i++){
+        $("#crystal"+i).attr("data-points", randomNumber(1,12));
+    };
+}
+
+//RANDOM NUMBER FUNCTION
+function randomNumber(min , max)
+{
+    return Math.round( (Math.random()*(max-min)) + min);     
+}
+
+function reStart(){
+    
+    match = randomNumber(19, 120);
+    $("#random").html(match);
+    console.log(random);
+    totalScore = 0;
+    $("#totalScore").html(totalScore);    
+    
+    randomCrystalValues();     
+}
